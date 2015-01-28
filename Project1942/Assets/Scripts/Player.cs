400e2bb322d92f35eeb2c652e2af70fd806a88c6
Callum <callum.waddell@btinternet.com>
2015-01-28 17:55:55 +0000

    Player Movement and Shooting
    added place holder sprites
    player moves forces and vel changed when off camera
    bullet moved with force and keeps players vel
    shooting sound added

---
+++ Project1942/Assets/Scripts/Player.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : Sprite {
+
+    float forceForward = 50;
+    float forceBackward = 100;
+    float forceSideways = 500;
+
+    public   Camera mainCamera;
+    public GameObject bulletPrefab;
+
+    float bulletForce = 100;
+
+    AudioSource soundShoot;
+    
+	// Use this for initialization
+	void Start () 
+    {
+        Init();
+	}
+
+    public override void Init()
+    {
+        base.Init();
+
+        soundShoot = GetComponent<AudioSource>();
+    }
+
+
+	// Update is called once per frame
+	void Update () 
+    {
+        Movement();
+        Shooting();
+	}
+
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigidbody2D.AddForce( - Vector3.right * forceSideways * Time.deltaTime );
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigidbody2D.AddForce(Vector3.right * forceSideways * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigidbody2D.AddForce(Vector3.up * forceForward * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rigidbody2D.AddForce(-Vector3.up * forceBackward * Time.deltaTime);
+        }
+    }
+
+    void OnBecameInvisible()
+    {
+        Vector3 dir = (mainCamera.transform.position - transform.position).normalized;
+        dir.z = 0;
+        rigidbody2D.velocity = dir * 10;
+    }
+
+
+    void Shooting()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject bullet =  Instantiate(bulletPrefab, transform.position, transform.rotation) as GameObject;
+
+            bullet.rigidbody2D.velocity = rigidbody2D.velocity;
+            
+            bullet.rigidbody2D.AddForce(Vector3.up  * bulletForce);
+
+            soundShoot.pitch = Random.Range(.9f, 1.1f);
+            soundShoot.Play();
+        }
+    }
+}
---
+++ Project1942/Assets/Scripts/Sprite.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sprite : MonoBehaviour
+{
+    float health = 100;
+
+
+    Animator animator;
+
+	// Use this for initialization
+	void Start () 
+    {
+        Init();
+	}
+	
+    public virtual void Init()
+    {     
+        animator = GetComponent<Animator>();
+    }
+
+
+	// Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   public void TakeDamage( float amount)
+    {
+        health -= amount;
+        health = Mathf.Clamp(health, 0, 100);
+
+        if (health < 0)
+        {
+            Death();
+        }
+    }
+
+   void Death()
+   {
+       Destroy(gameObject);   
+   }
+
+    
+}
